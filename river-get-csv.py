8ce30f008aa0670e5bd4057c4d88ad20dd94bd8b
alecwu44743 <alec.wu44743@gmail.com>
2022-10-21 01:47:17 +0800

    [Fixed] Automatically open all files and write to CSV - Oct 21, 2022

--- river-get-csv.py
+++ river-get-csv.py
@@ -2,7 +2,8 @@ import pandas as pd
 import csv
 import os
 
-file_path = "./"
+root_path = "./Tamsui-River-DATA"
+save_path = "./result"
 
 filename = "river.csv"
 csv_header = ['x', 'y']
@@ -50,35 +51,47 @@ def get_data(data):
 
 
 if __name__ == '__main__':
-    for river_file in os.listdir():
-        if river_file.startswith("T"):
-            
-            file = open(river_file, 'r')
-            filename = river_file + ".csv"
-            
-            lines = file.readlines()
-            with open(filename, 'w') as csvfile:
-                csvwriter = csv.writer(csvfile) 
-                csvwriter.writerow(csv_header)
-            
-            line_index = 0
-            for data in lines:
-                if line_index >= 2:
-                    if len(data) < 2:
-                        break
-                    
-                #     print(ll.strip())
-                #     print(len(ll))
-                    get_data(data.strip())
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    
+    for dirdir in os.listdir(root_path):
+        # if dirdir is .DS_Store to continue
+        if dirdir == ".DS_Store":
+            continue
+        
+        now_to_path = "./Tamsui-River-DATA/" + dirdir
+        for river_file in os.listdir(now_to_path):
+            now_to_file = now_to_path + "/" + river_file
+            if not river_file == "T9612010.A" and not os.path.isdir(now_to_file) and not river_file.endswith(".DS_Store") and not river_file.endswith(".git") and not river_file.endswith(".gitignore") and not river_file.endswith(".zip") and not river_file.endswith(".CSV") and not river_file.endswith(".csv") and not river_file.endswith(".gitignore") and not river_file.endswith(".ZIP") and not river_file.endswith(".EXE") and not river_file.endswith(".txt") and not river_file.endswith(".TXT") and not river_file.endswith(".doc") and not river_file.endswith(".INT") and not river_file.endswith(".OUT") and not river_file.endswith(".DAT") and not river_file.endswith(".py"):
                 
+                filename = save_path + "/" + river_file + ".csv"
+                file = open(now_to_file, 'r')
+                
+                print("Processing " + now_to_file + "...")
+                
+                lines = file.readlines()
+                with open(filename, 'w') as csvfile:
+                    csvwriter = csv.writer(csvfile) 
+                    csvwriter.writerow(csv_header)
+                
+                line_index = 0
+                for data in lines:
+                    if line_index >= 2:
+                        if len(data) < 2:
+                            break
+                        
+                    #     print(ll.strip())
+                    #     print(len(ll))
+                        get_data(data.strip())
                     
-                line_index += 1
-            
-            
-            dataFrame = pd.read_csv(filename)
-            dataFrame.sort_values('x' , axis=0, ascending=True,inplace=True, na_position='first')
-            
-            print(dataFrame)
-            
-            os.remove(filename)
-            dataFrame.to_csv(filename, index=False)
+                        
+                    line_index += 1
+                
+                
+                dataFrame = pd.read_csv(filename)
+                dataFrame.sort_values('x' , axis=0, ascending=True,inplace=True, na_position='first')
+                
+                # print(dataFrame)
+                
+                os.remove(filename)
+                dataFrame.to_csv(filename, index=False)
