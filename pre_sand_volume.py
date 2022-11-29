e0d532b363a0fc292bf16df4d65e1fa01c4df984
alecwu44743 <alec.wu44743@gmail.com>
2022-11-29 13:42:49 +0800

    [Fix] optimization - Nov 29, 2022

--- pre_sand_volume.py
+++ pre_sand_volume.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt  # for 畫圖用
+# import matplotlib.image as mpimg
+from PIL import Image
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
@@ -7,8 +9,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
-
-nor_data = pd.DataFrame()
 
 
 def readTrain():
@@ -19,8 +19,6 @@ def readTrain():
 def normalization(training_set):
     sc = MinMaxScaler(feature_range = (0, 1))
     training_set_scaled = sc.fit_transform(training_set)
-    
-    nor_data = training_set_scaled
     
     return training_set_scaled
 
@@ -42,12 +40,6 @@ def buildTrain(train, pastDay=30, futureDay=5, n=10):
             ans.append(temp)
             
         Y_train.append(ans)
-        
-        # print("y_train")
-        # print(train.iloc[i+pastDay:i+pastDay+futureDay]["sand"])
-        # print(len(Y_train))
-        # print()
-    
     
     return np.array(X_train), np.array(Y_train)
 
@@ -76,72 +68,31 @@ def buildLSTM(x_train, y_train):
     regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
     regressor.fit(x_train, y_train, epochs = 1000, batch_size = 32)
     
-
-    dataset_train = pd.read_csv('./typhoon06/2012su.csv')
-    inputs = normalization(dataset_train)
-    
     dataset_test = pd.read_csv("./typhoon06/2012su.csv")
     real_sand = dataset_test.iloc[0:75]["sand"]
     
-    # ssc = MinMaxScaler(feature_range = (0, 1))
+    dataset_test = normalization(dataset_test)
+    dataset_test = pd.DataFrame(dataset_test, columns=['h', 'flow', 'sand'])
     
-    # dataset_total = pd.concat((dataset_train['sand'], dataset_test['sand']), axis = 0)
-    # print(dataset_total)
-    # print(dataset_total.shape)
-    # inputs = dataset_total[len(dataset_total) - len(dataset_test) - 5:].values # train_norm = train_norm.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-    # inputs = inputs.reshape(-1,1)
-    # inputs = sc.fit_transform(inputs)
-    # inputs = sc.transform(inputs)
-    # print(type(inputs))
-    # print(inputs)
-    # print(inputs.shape)
+    X_test = []
+    for i in range(dataset_test.shape[0]-3):
+        X_test.append(np.array(dataset_test.iloc[i:i+3]))
     
-    # inputs = dataset_test.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-    
-    
-    # X_test, Y_test = buildTrain(normalization(dataset_test), 1, 1, 0)
-    # for i in range(inputs.shape[0]-1-3):
-    #     X_test.append(np.array(inputs.iloc[i:i+3])) # X_train.append(np.array(train.iloc[i:i+pastDay]))
-    
-    #     # print("y_train")
-    #     # print(train.iloc[i+pastDay:i+pastDay+futureDay]["sand"])
-    #     # print(len(Y_train)) 
-    #     # print()
-    X_test, Y_test = buildTrain(normalization(dataset_test), 3, 1, 0)
+    # X_test, Y_test = buildTrain(normalization(dataset_test), 3, 1, 0)
     X_test = np.array(X_test)
-    print(X_test)
-    print(X_test.shape)
-    
-    # X_test = nor_data
     # print(X_test)
     # print(X_test.shape)
-    
-    # sc_X_test = MinMaxScaler(feature_range = (0, 1)).fit(X_test)
-    # X_test = sc_X_test.transform(X_test)
     
     sc_Y_test = MinMaxScaler(feature_range = (0, 1)).fit(np.array(real_sand).reshape(-1, 1))
     
-
-    # # for i in range(3, len(inputs)):
-    # #     X_test.append(inputs[i-3:i,])
-    # X_test = np.array(X_test)
-    # print(X_test)
-    # print(X_test.shape)
-    # X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    
-    # X_test = regressor.fit(X_test)
     predicted_sand = regressor.predict(X_test)
-    print(type(predicted_sand))
-    print(predicted_sand.shape)
+    # print(type(predicted_sand))
+    # print(predicted_sand.shape)
     
-    print(predicted_sand)
+    # print(predicted_sand)
     
     predicted_sand = pd.DataFrame(predicted_sand)
     predicted_sand = sc_Y_test.inverse_transform(predicted_sand)
-    
-    print(predicted_sand)
-    
-    # # predicted_sand = sc.inverse_transform(predicted_sand)
     
     plt.plot(real_sand, color = 'red', label = 'Real sand')
     plt.plot(predicted_sand, color = 'blue', label = 'Predicted sand')
@@ -150,8 +101,10 @@ def buildLSTM(x_train, y_train):
     plt.ylabel('sand')
     plt.legend()
     # plt.show()
-    
     plt.savefig('./result_image/sand-20221129.png', dpi=1000)
+    
+    image = Image.open('./result_image/sand-20221129.png')
+    image.show()
 
 
 
@@ -161,10 +114,10 @@ if __name__ == "__main__":
     training_set_scaled = normalization(training_set)
     
     x_train, y_train = buildTrain(training_set_scaled, 3, 1)
-    print(x_train)
-    print(y_train)
-    print(x_train.shape)
-    print(y_train.shape)
+    # print(x_train)
+    # print(y_train)
+    # print(x_train.shape)
+    # print(y_train.shape)
     # (73, 3, 3)
     # (73, 1, 1)
     
